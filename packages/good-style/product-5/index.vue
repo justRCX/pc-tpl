ef958d503d6cc6e9ea69bfc6ce8a9b42e1ffa613
chenhaijia <1286208191@qq.com>
2020-12-10 18:25:55 +0800

    修改

---
+++ packages/good-style/product-5/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="goods-wapper">
+      <div
+        v-for="(item,index) in list"
+        :key="index"
+        class="goods-wapper-item"
+      >
+        <goods-item></goods-item>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import GoodsItem from '../component/goods-item'
+  export default {
+    components: {
+      GoodsItem
+    },
+    props: ['config', 'list'],
+    data() {
+      return {
+
+      }
+    },
+    mounted() {
+
+    }
+  }
+</script>
+<style scoped lang="scss">
+  @import "../../util.scss";
+  .goods-wapper {
+    display: flex;
+    flex-wrap: wrap;
+    background: #fff;
+    &-item {
+      width: to320(220);
+      flex-shrink: 0;
+    }
+  }
+</style>
